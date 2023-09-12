80416577f5b45d5b803329b76e8a56308a192832
daniel <danieljijoca@outlook.com>
2023-09-12 19:10:35 -0300

    firstCommit

--- ListaDeTarefas/src/test/java/com/example/ListaDeTarefas/controllers/TarefaControllerTest.java
+++ ListaDeTarefas/src/test/java/com/example/ListaDeTarefas/controllers/TarefaControllerTest.java
@@ -1,54 +1,119 @@
 package com.example.ListaDeTarefas.controllers;
 
+import com.example.ListaDeTarefas.Service.TaferaService;
+import com.example.ListaDeTarefas.exceptions.TarefaValidationExceptions;
 import com.example.ListaDeTarefas.model.Tarefa;
-import com.example.ListaDeTarefas.repository.ITarefa;
+import com.example.ListaDeTarefas.controllers.TarefaController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalManagementPort;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class TarefaControllerTest {
-    @LocalServerPort
-    private int port;
-    @Autowired
-    private TestRestTemplate testRestTemplate;
-    @Autowired
-    private ITarefa tarefaRepository;
+import static org.mockito.Mockito.*;
+
+public class TarefaControllerTest {
+
+    private TarefaController tarefaController;
+
+    @Mock
+    private TaferaService tarefaService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        tarefaController = new TarefaController(tarefaService);
+    }
 
     @Test
-    void listarTarefas() {
+    public void testListarTarefas() {
+        List<Tarefa> tarefas = new ArrayList<>();
+        when(tarefaService.listarTarefas()).thenReturn(tarefas);
+
+        ResponseEntity<List<Tarefa>> response = tarefaController.listarTarefas();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(tarefas, response.getBody());
+    }
+
+    @Test
+    public void testBuscarTarefaPorId_Encontrada() {
+        Long id = 1L;
         Tarefa tarefa = new Tarefa();
-        tarefa.setTitulo("Tarefa");
-        tarefaRepository.save(tarefa);
-        ResponseEntity<String>  forEntity = testRestTemplate.getForEntity("http://localhost:" + port + "/tarefas",String.class);
-        assertEquals(HttpStatus.OK,forEntity.getStatusCode());
-        assertTrue(forEntity.getBody().contains("Tarefa"));
+        when(tarefaService.buscarTarefaPorId(id)).thenReturn(tarefa);
 
-        System.out.print("RESULTADO: "+ forEntity.getBody());
+        ResponseEntity<?> response = tarefaController.buscarTarefaPorId(id);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(tarefa, response.getBody());
     }
 
     @Test
-    void buscarTarefaPorId() {
+    public void testBuscarTarefaPorId_NaoEncontrada() {
+        Long id = 1L;
+        when(tarefaService.buscarTarefaPorId(id)).thenReturn(null);
+
+        ResponseEntity<?> response = tarefaController.buscarTarefaPorId(id);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Tarefa com o ID 1 não encontrada, procure por outro ID.", response.getBody());
     }
 
     @Test
-    void criarTarefa() {
+    public void testCriarTarefa_Valida() {
+        Tarefa tarefa = new Tarefa();
+        when(tarefaService.adicionarTarefa(tarefa)).thenReturn(tarefa);
+
+        ResponseEntity<Tarefa> response = tarefaController.criarTarefa(tarefa);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(tarefa, response.getBody());
     }
 
     @Test
-    void editarPokemon() {
+    public void testCriarTarefa_Invalida() {
+        Tarefa tarefa = new Tarefa();
+        tarefa.setTitulo("");
+        when(tarefaService.adicionarTarefa(tarefa)).thenThrow(TarefaValidationExceptions.class);
+
+        assertThrows(TarefaValidationExceptions.class, () -> tarefaController.criarTarefa(tarefa));
     }
 
     @Test
-    void excluirTarefa() {
+    public void testEditarTarefa() {
+        Tarefa tarefa = new Tarefa();
+        when(tarefaService.editarTarefa(tarefa)).thenReturn(tarefa);
+
+        ResponseEntity<Tarefa> response = tarefaController.editarPokemon(tarefa);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(tarefa, response.getBody());
+    }
+
+    @Test
+    public void testExcluirTarefa_Encontrada() {
+        Long id = 1L;
+        when(tarefaService.excluirTarefa(id)).thenReturn(true);
+
+        ResponseEntity<String> response = tarefaController.excluirTarefa(id);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("A tarefa com o ID 1 foi apagada com sucesso.", response.getBody());
+    }
+
+    @Test
+    public void testExcluirTarefa_NaoEncontrada() {
+        Long id = 1L;
+        when(tarefaService.excluirTarefa(id)).thenReturn(false);
+
+        ResponseEntity<String> response = tarefaController.excluirTarefa(id);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("A tarefa com o ID 1 não foi encontrada.", response.getBody());
     }
 }
